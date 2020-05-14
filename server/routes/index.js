795bf9555c44d132b4461090cfcb0ebd7cf6e334
João Victor dos Santos Rocha <joaorocha.webtech@gmail.com>
2020-05-14 00:48:24 -0400

    Gerado ambiente individual para o front e back e testado a conexao entre ambos

---
+++ server/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+/* GET home page. */
+
+// options =  {
+//   url: "https://api.github.com/repositories",
+//   headers: {
+//       "User-Agent": "joaovic030",  // Your Github ID or application name
+//       "Authorization": "8bb65004ce7686b620b9e6754564a5f7ed5a2bf2"
+//   }
+// }
+// request.get(options)
+//   .on('response', function (response) {
+//       console.log(response.statusCode);
+//       console.log(JSON.stringify(response));
+//       repositories = JSON.stringify(response)
+//   });
+
+async function githubRequest() {
+  try {
+    return await axios.get(`https://api.github.com/repositories`, {
+      headers: {
+        authorization: `token 8bb65004ce7686b620b9e6754564a5f7ed5a2bf2`
+      }
+    });
+  } catch (error) {
+    console.error(error)
+  }
+}
+let repos = []
+githubRequest().then(response => { repos = response.data});
+
+router.get('/', function(req, res, next) {
+  res.send({ repositories: repos })
+});
+
+module.exports = router;
